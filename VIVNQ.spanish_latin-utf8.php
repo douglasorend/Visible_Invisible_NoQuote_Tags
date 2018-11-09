781317af32e96382a3f8458381fd37de27e36b28
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-11-09 12:04:45 -0600

    v3.5 - February 13th, 2018
    v3.5 - February 13th, 2018
    o Added Spanish Latin translation, courtsey of [url=https://www.simplemachines.org/community/index.php?action=profile;u=322597]Rock Lee[/url].
    o Added translators section to [b]README.txt[/b].
    o Added [b]LICENSE.txt[/b] to satisfy licensing concerns.

---
+++ VIVNQ.spanish_latin-utf8.php
@@ -0,0 +1,71 @@
+<?php
+/**********************************************************************************
+* VIVNQ.spanish_latin-utf8.php
+***********************************************************************************
+* This mod is licensed under the 2-clause BSD License, which can be found here:
+*	http://opensource.org/licenses/BSD-2-Clause
+***********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but      *
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
+***********************************************************************************
+* Spanish translation by Rock Lee (https://www.bombercode.org) Copyright 2014-2018*
+***********************************************************************************/
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+$txt['VIVNQ_Settings'] = 'Configuraciones VIVNQ';
+$txt['VIVNQ_description'] = 'Desde aquí puede administrar los accesos directos definidos por el usuario para <strong>visible</strong> y <strong>invisible</strong> bbcodes. También puede controlar quién puede usar los bbcodes definidos por el mod VIVNQ.';
+$txt['VIVNQ_shortcuts'] = 'Atajos';
+$txt['VIVNQ_new_tag'] = 'Crear nueva etiqueta';
+
+$txt['permissiongroup_VIVNQ'] = $txt['permissiongroup_simple_VIVNQ'] = 'Visible, Invisible y sin cita';
+$txt['permissionname_VIVNQ_use_visible'] = 'Permitir el uso del &quot;visible&quot; tag';
+$txt['permissionhelp_VIVNQ_use_visible'] = 'Al marcar esta opción, los usuarios de este grupo de miembros pueden usar &quot;visible&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_quote_visible'] = 'Citar el contenido de &quot;visible&quot; tag';
+$txt['permissionhelp_VIVNQ_quote_visible'] = 'Al marcar esta opción, los usuarios de este grupo de miembros pueden citar el &quot;visible&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_use_invisible'] = 'Permitir el uso de la etiqueta &quot;invisible&quot;';
+$txt['permissionhelp_VIVNQ_use_invisible'] = 'Al marcar esta opción, los miembros de este grupo de miembros pueden usar &quot;invisible&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_quote_invisible'] = 'Citar el contenido de &quot;invisible&quot; tag';
+$txt['permissionhelp_VIVNQ_quote_invisible'] = 'Al marcar esta opción, los usuarios de este grupo de miembros pueden citar el &quot;invisible&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_use_noquote'] = 'Permitir el uso de la &quot;noquote&quot; tag';
+$txt['permissionhelp_VIVNQ_use_noquote'] = 'Al marcar esta opción, los miembros de este grupo de miembros pueden usar the &quot;noquote&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_quote_noquote'] = 'Citar el contenido de &quot;noquote&quot; tag';
+$txt['permissionhelp_VIVNQ_quote_noquote'] = 'Al marcar esta opción, los usuarios de este grupo de miembros pueden citar el &quot;innoquote&quot; bbcode tag para filtrar quién puede ver el mensaje contenido en las etiquetas.<br/><br/>Al desmarcar esta opción, el foro eliminará las etiquetas del mensaje antes de enviarlo a la base de datos.';
+$txt['permissionname_VIVNQ_toggle_filter'] = 'Alternar el filtrado de &quot;visible&quot; &amp; &quot;invisible&quot; tag';
+$txt['permissionhelp_VIVNQ_toggle_filter'] = 'De forma predeterminada, solo los administradores, moderadores y el autor original de la publicación tienen derecho a ver cómo el foro procesará el contenido no filtrado..<br/><br/>Al marcar esta casilla, el usuario puede ver el contenido de &quot;visible&quot;, &quot;invisible&quot;, y &quot;noquote&quot; etiquetas haciendo clic en el &quot;Filter On&quot; o &quot;Filter Off&quot; botones cerca del &quot;Modificar&quot; y &quot;Citar&quot;.';
+
+$txt['VIVNQ_use_visible'] = 'Puede usar el &quot;visible&quot; tag:';
+$txt['VIVNQ_quote_visible'] = 'Puede citar &quot;visible&quot; tag entre comillas:';
+$txt['VIVNQ_use_invisible'] = 'Puede usar el &quot;invisible&quot; tag:';
+$txt['VIVNQ_quote_invisible'] = 'Puede citar &quot;invisible&quot; tag entre comillas:';
+$txt['VIVNQ_use_noquote'] = 'Puede usar el &quot;noquote&quot; tag:';
+$txt['VIVNQ_quote_noquote'] = 'Puede citar &quot;noquote&quot; tag entre comillas:';
+$txt['VIVNQ_toggle_filter'] = 'Puede ver cuando se establecen las condiciones:';
+
+$txt['VIVNQ_no_shortcuts'] = 'Aún no se han definido accesos directos.';
+$txt['VIVNQ_Tag'] = 'Etiqueta';
+$txt['VIVNQ_Replacement'] = 'BBCode Reemplazo';
+$txt['VIVNQ_Modify'] = 'Modificar';
+$txt['VIVNQ_Confirm'] = '¿Estás seguro que quieres eliminar este bbcode? Cualquier uso de este bbcode debe eliminarse manualmente del foro.';
+$txt['VIVNQ_Delete'] = 'Borrar';
+
+$txt['VIVNQ_tag'] = 'Atajo BBCode:';
+$txt['VIVNQ_which'] = 'Acceso directo para la etiqueta:';
+$txt['VIVNQ_params'] = '¿El acceso directo usa qué parámetros?';
+$txt['VIVNQ_Param_u'] = 'Número(s) de identificación de miembro permitidos:';
+$txt['VIVNQ_Param_g'] = 'Número(s) de ID de grupo de miembros permitidos:';
+$txt['VIVNQ_Param_min_posts'] = 'Número mínimo de publicaciones:';
+$txt['VIVNQ_Param_max_posts'] = 'Número máximo de publicaciones:';
+$txt['VIVNQ_Param_guests'] = '¿El usuario es un invitado?';
+$txt['VIVNQ_Param_members'] = '¿El usuario es un miembro?';
+$txt['VIVNQ_Param_banned'] = '¿El usuario está prohibido?';
+$txt['VIVNQ_Param_username'] = 'Nombres de usuario:';
+$txt['VIVNQ_Param_user'] = 'Usuario:';
+$txt['VIVNQ_Param_group'] = 'Grupos de miembros permitidos:';
+$txt['VIVNQ_Param_lang'] = 'Idioma(s):';
+$txt['VIVNQ_Param_karma'] = '¿Karma mínimo requerido?';
+$txt['VIVNQ_Param_replied'] = 'Tema para verificar la respuesta:';
+$txt['VIVNQ_Param_warning'] = 'Nivel de advertencia de los miembros:';
+
+?>
